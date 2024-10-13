a25095eb9f66140373480411706e29664c79b942
Mwangale Isaac <isaac.mwangale7@gmail.com>
2024-10-13 14:22:38 +0300

    adding changes

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-I = (principal * rate * time)
+interest = (principal * rate * time)
 
-print("The simple interest is: ", I)
+print("The simple interest is: ", interest)
