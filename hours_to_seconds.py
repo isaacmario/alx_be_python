3a130aa1a048bd2c647c95bea3cdaeade3a82104
Mwangale Isaac <isaac.mwangale7@gmail.com>
2024-10-13 13:00:07 +0300

    add files to Alx_be_at_python

--- hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-#converting hours to seconds 
-hours = 2
-
-seconds = hours * 3600
-print("2 hours is ",seconds, "seconds")
--- rectangle_area.py
+++
@@ -1,6 +0,0 @@
-#Finding area of the circle
-length = 10
-width = 5
-
-Area = (length * width)
-print(Area)
